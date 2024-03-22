f38d8433f86c03546112ccee7c4007b0dc147e27
root <root@Jacks_Laptop.localdomain>
2024-03-22 22:31:07 +1100

    Begin moving documentation to a separate notebook and add dependencies to project

---
+++ doc/variable_explorer_documentation.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.19.40
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 32e9249c-e83e-11ee-0c7d-f3dd42ccea41
+begin
+	import Pkg; Pkg.offline(true)
+	Pkg.add(url="https://github.com/JackDevine/PlutoVariableExplorer.jl")
+	using PlutoVariableExplorer
+end
+
+# ╔═╡ Cell order:
+# ╠═32e9249c-e83e-11ee-0c7d-f3dd42ccea41
